f1d086b0151d2ef2e4d7a8c68d7fba72cc21b34a
bpoinder85 <poindexter.brandon85@gmail.com>
2017-11-15 20:19:49 -0700

    First working version of mazeSolver.js

--- mazeSolver.js
+++ mazeSolver.js
@@ -6,14 +6,14 @@ var maze = ["############S##############",
             "#     # #                 #",
             "#     # ###################",
             "#     #                   E",
-            "#     #####################",
-            "#                         #",
-            "#                         #",
-            "#                         #",
-            "#                         #",
-            "#                         #",
-            "#                         #",
-            "#                         #",
+            "#     # ###################",
+            "#     # #                 #",
+            "#     # #                 #",
+            "#     # #                 #",
+            "#     # #                 #",
+            "#     # #                 #",
+            "#     # #                 #",
+            "#     # #                 #",
             "###########################"];
 
 var start;
@@ -35,31 +35,58 @@ function parseMap(map) {
   // find dimensions
   height = map.length;
   width = map[0].length;
-  findPaths(start);
 }
 
-function findPaths(y, x) {
-  var next = [];
+function findPaths(pos) {
+  var y = pos[0];
+  var x = pos[1];
 
   // add current position to visited list
   visited.push(y); visited.push(x);
 
   //check if current space is an exit
+  console.log("Current position is " + pos);
   if(maze[y].charAt(x) == "E") {
+    console.log("Found path");
     return true;
   } else {
     //check for validity of all directions, if valid, add to queue. Then, rerun findPaths.
+    if(y-1 >= 0) {
+      if (maze[y-1].charAt(x) != "#" && checkVisited(y-1, x)) {
+        queue.push(y-1); queue.push(x);
+      }
+    }
+    if(y+1 < height) {
+      if (maze[y+1].charAt(x) != "#" && checkVisited(y+1, x)) {
+        queue.push(y+1); queue.push(x);
+      }
+    }
+    if(x-1 >= 0) {
+      if (maze[y].charAt(x-1) != "#" && checkVisited(y, x-1)) {
+        queue.push(y); queue.push(x-1);
+      }
+    }
+    if(x+1 < width) {
+      if (maze[y].charAt(x+1) != "#" && checkVisited(y, x+1)) {
+        queue.push(y); queue.push(x+1);
+      }
+    }
+    // recursion
+    nextY = queue.shift();
+    nextX = queue.shift();
+    nextPos = [nextY, nextX];
+    findPaths(nextPos);
   }
-  
 }
 
 function checkVisited(y, x) {
   for (var i=0; i<visited.length; i=i+2) {
     if (y == visited[i] && x == visited[i+1]) {
-      return true;
+      return false;
     } 
-    return false;
   }
+  return true;
 }
 
 parseMap(maze);
+findPaths(start);
