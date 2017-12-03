889d1fa3222c25fb51de7e12098bd82efba414a9
bpoinder85 <poindexter.brandon85@gmail.com>
2017-12-03 15:36:59 -0700

    Changed main files to match prior branched files.

--- mazeSolver.js
+++ mazeSolver.js
@@ -1,38 +1,35 @@
-var maze = ["############S##############",
-            "#     ###### #            #",
-            "#     #      #            #",
-            "#     # ######            #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # ###################",
-            "#     #                   E",
-            "#     # ###################",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "#     # #                 #",
-            "###########################"];
-
-var start;
-var height = maze.length;
-var width = maze[0].length;
+var map = [];
 var visited = [];
 var queue = [];
+var start = [0, 0];
 
-function parseMap(map) {
-  // find starting position
-  for (var i=0; i<map.length; i++) {
-    for (var j=0; j<map[i].length; j++) {
-      if (map[i].charAt(j) == "S") {
-        start = [i, j];
-        document.getElementById("y" + i + "x" + j).innerHTML = "S";
-      }
-      document.getElementById("y" + i + "x" + j).innerHTML = map[i].charAt(j);
-      if (map[i].charAt(j) == "#") {
-        document.getElementById("y" + i + "x" + j).style.backgroundColor = 'black';
+function createMaze() {
+  var hyperString = "";
+
+  for (var i=0; i<50; i++) {
+    hyperString = hyperString.concat("<tr>");
+    for (var j=0; j<50; j++) {
+      hyperString = hyperString.concat("<td class='cell' id='y" + i + "x" + j + "'></td>");
+    }
+    hyperString = hyperString.concat("</tr>");
+  }
+
+  document.getElementById("mazeTable").innerHTML = hyperString;
+}
+
+function fillMap() {
+
+  for(var i=0; i<50; i++) {
+    map.push("");
+    for (var j=0; j<50; j++) {  
+      if($('#y' + i + 'x' + j).css('background-color') == 'rgb(0, 128, 128)') {
+        map[i] = map[i].concat("#");
+      } else if ($('#y' + i + 'x' + j).css('background-color') == 'rgb(0, 0, 0)') {
+        map[i] = map[i].concat(" ");
+      } else if ($('#y' + i + 'x' + j).css('background-color') == 'rgb(0, 0, 255)') {
+        map[i] = map[i].concat("S");
+      } else {
+        map[i] = map[i].concat("E");
       }
     }
   }
@@ -48,28 +45,28 @@ function findPaths(pos) {
   //check if current space is an exit
   console.log("Current position is " + pos);
   document.getElementById("y" + y + "x" + x).style.backgroundColor = "red";
-  if(maze[y].charAt(x) == "E") {
+  if(map[y].charAt(x) == "E") {
     console.log("Found path");
     return true;
   } else {
     //check for validity of all directions, if valid, add to queue. Then, rerun findPaths.
     if(y-1 >= 0) {
-      if (maze[y-1].charAt(x) != "#" && checkVisited(y-1, x)) {
+      if (map[y-1].charAt(x) != "#" && checkVisited(y-1, x)) {
         queue.push(y-1); queue.push(x);
       }
     }
-    if(y+1 < height) {
-      if (maze[y+1].charAt(x) != "#" && checkVisited(y+1, x)) {
+    if(y+1 < 50) {
+      if (map[y+1].charAt(x) != "#" && checkVisited(y+1, x)) {
         queue.push(y+1); queue.push(x);
       }
     }
     if(x-1 >= 0) {
-      if (maze[y].charAt(x-1) != "#" && checkVisited(y, x-1)) {
+      if (map[y].charAt(x-1) != "#" && checkVisited(y, x-1)) {
         queue.push(y); queue.push(x-1);
       }
     }
-    if(x+1 < width) {
-      if (maze[y].charAt(x+1) != "#" && checkVisited(y, x+1)) {
+    if(x+1 < 50) {
+      if (map[y].charAt(x+1) != "#" && checkVisited(y, x+1)) {
         queue.push(y); queue.push(x+1);
       }
     }
@@ -93,28 +90,31 @@ function checkVisited(y, x) {
   return true;
 }
 
-function drawMap() {
-  var tableHTML = "";
+$(document).ready(function() {
+  createMaze();
+  
+  $("#y0x0").css('background-color', 'blue');
+  $("#y49x49").css('background-color', 'yellow');
+  // Event handler for drawing paths.
+  var isDown = false;
+  var curID;
 
-  for (var i=0; i<height; i++) {
-    tableHTML = tableHTML.concat("<tr>");
-    for (var j=0; j<width; j++) {
-      tableHTML = tableHTML.concat("<td class='cell' id='y" + i + "x" + j + "'></td>");
+  $(document).mousedown(function() {
+    isDown = true;
+  })
+  .mouseup(function() {
+    isDown = false;
+  });
+
+  $('.cell').mouseover(function () {
+      curID = $(this).attr('id');
+      if(isDown) {
+        $("#" + curID).css('background-color', 'black');
+      }
     }
-    tableHTML = tableHTML.concat("</tr>");
-  }
+  );
+});
 
-  document.getElementById("mazeTable").innerHTML = tableHTML;
-}
-
-drawMap();
-parseMap(maze);
-findPaths(start);
-
-
-/* TODO - Create a feature where the table is initially blank and the user draws (via a 
-mouse-down event handler when that's true over the current cell) the maze, and it edits the
-maze array. Once the maze array has been edited to match the visual maze on the page, the
-user will also input the start and end points on the map. Then the findPaths() function will
-start (via a button) to visualize the solving of the maze. */
-
+/*
+TODO - Prevent overlapping of yellow and blue start and exit points.
+*/ 
